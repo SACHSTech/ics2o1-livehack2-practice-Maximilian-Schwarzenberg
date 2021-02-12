2cda1d4e17804889dd95878fe5b933be381f7d75
Maximilian Schwarzenberg <maximilian.schwarzenberg23@ycdsbk12.ca>
2021-02-12 14:22:29 +0000

    Finished the assignment

--- problem1.py
+++ problem1.py
@@ -1,1 +1,16 @@
+"""
+Name: Max S.
+Purpose: To identify a right angle triangle from its side lengths. 
+Due date: ?
+"""
 
+ # User inputs side lengths
+length1 = int(input("Enter the length of side 1: "))
+length2 = int(input("Enter the length of side 2: "))
+length3 = int(input("Enter the length of side 3: "))
+
+ # Calculations and display
+if length1 ** 2 + length2 ** 2 == length3 ** 2:
+  print("The triangle with side lengths",length1,",",length2,", and",length3,"form a right-angled triangle.")
+else: 
+  print("The triangle with side lengths",length1,",",length2,", and",length3," does not form a right-angled triangle.")
--- problem2.py
+++ problem2.py
@@ -1,1 +1,17 @@
+"""
+Name: Max S.
+Purpose: To find out if you are able to go on vacation during the summer. 
+Due date: ?
+"""
 
+ # Input of data
+mark = int(input("What is your average? (%): "))
+earnings_before = int(input("What is your earnings before the summer?: $"))
+
+ # Processing and Display of the data
+if mark >= 80 and earnings_before >= 500: 
+  print("You can now for vacation to Europe!")
+elif mark >= 80 and earnings_before < 500: 
+  print("You can now for vacation to California!")
+else: 
+  print("You did not achieve an average of 80% or over or earn $500 so you can not go on vacation. You need",80 - mark,"more of an average and $",500 - earnings_before,"more money.")
